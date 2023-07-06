a920a9747898586eb7dc6c6cfc40ffe6bd048845
azaespa <pauljamesazanes@gmail.com>
2023-07-07 04:27:45 +0800

    Script to add new text element
    Added script to identify if there's already a section element. If yes, every new text element will just append to that. If no, it will create a section element first among the created text element

--- index.js
+++ index.js
@@ -1,13 +1,56 @@
-const mainTag = document.querySelector("main");
+const main = document.querySelector("main");
+const createTextBtn = document.getElementById("create-text");
 
-const element = mainTag.querySelector(".element");
+let section = document.getElementById("section");
 
-function handleClickElement() {
-    console.log("Element clicked");
+createTextBtn.addEventListener("click", () => {
+    // check if there's already a section element
+    // if yes, append to that.
+    // if no, create one.
+    if (!main.contains(section)) {
+        createSection();
+    }
+
+    const element = document.createElement("div");
+    const elementBtn = document.createElement("button");
+    element.classList.add("element");
+    elementBtn.id = "element-btn";
+    elementBtn.innerText = "Text";
+    
+    const text = document.createElement("p");
+    
+    text.classList.add("text");
+    text.contentEditable = "true";
+    text.innerText = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem dolore, alias, numquam enim ab voluptate id quam harum ducimus cupiditate similique quisquam et deserunt, recusandae.";
+    
+    element.append(elementBtn);
+    element.append(text);
+    createRowAndAppend(element);
+})
+
+function createRowAndAppend(element) {
+    const row = document.createElement("div");
+    const rowBtn = document.createElement("button");
+    
+    row.classList.add("row");
+    rowBtn.id = "row-btn";
+    rowBtn.innerText = "Row";
+    
+    row.append(rowBtn);
+    row.append(element);
+    section.append(row);
 }
 
-function init(){
-    element.addEventListener("click", handleClickElement);
+function createSection() {
+    const sec = document.createElement("section");
+    const secBtn = document.createElement("button");
+    
+    sec.id = "section";
+    secBtn.id = "section-btn";
+    secBtn.innerText = "Section";
+    
+    sec.append(secBtn);
+    main.append(sec);
+    
+    section = document.getElementById("section");
 }
-
-init();
